48c7f509e40e25debd3afee26a2145de9e955d49
Gasangwa Thomas <thomasgasangwa@gmail.com>
2022-05-18 11:21:34 +0300

    add another js file containing the js work for contact validation form and edit the index html on contact-part

---
+++ validation.js
@@ -0,0 +1,10 @@
+//Validating the email
+document.querySelector('#form').addEventListener('submit', (e) => {
+	let valid = true;
+  
+	const responseContainer = document.querySelector('#warning-message');
+  
+	const emailArray = document.querySelector('#email').value.split('');
+  
+
+  });
