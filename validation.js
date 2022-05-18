8222635f07e35c8a6bbae79d1adce4e509297609
AngomRanjan <angomranjan@gmail.com>
2022-05-18 14:09:52 +0530

    Added Email Validation

--- validation.js
+++ validation.js
@@ -6,5 +6,12 @@ document.querySelector('#form').addEventListener('submit', (e) => {
   
 	const emailArray = document.querySelector('#email').value.split('');
   
-
-  });
+	for (let i = 0; i < emailArray.length; i += 1) { 
+		if ( emailArray[i] !== emailArray[i].toLowerCase() && emailArray[i] !== '@' && emailArray[i] !== '.') 
+		{ valid = false; } 
+	} 
+		if (!valid) { e.preventDefault(); 
+			responseContainer.innerText = 'Email provided is invalid: Please your email address should be in lower case only !';
+			responseContainer.style.color = 'yellow';
+		}
+	 })
